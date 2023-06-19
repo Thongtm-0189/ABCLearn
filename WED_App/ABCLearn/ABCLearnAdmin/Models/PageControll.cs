fce1e9371f1b6fa903d1b82b2ae78ef8cddb224a
Trần Minh Thông <100175655+Thongtm-0189@users.noreply.github.com>
2023-06-19 14:43:28 +0700

    Delete WED_App/ABCLearn directory

--- WED_App/ABCLearn/ABCLearnAdmin/Models/PageControll.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ABCLearn.Models
-{
-    public class PageControll
-    {
-        public string page { get; set; }
-        public string control { get; set; }
-        public int Id { get; set; }
-    }
-}
